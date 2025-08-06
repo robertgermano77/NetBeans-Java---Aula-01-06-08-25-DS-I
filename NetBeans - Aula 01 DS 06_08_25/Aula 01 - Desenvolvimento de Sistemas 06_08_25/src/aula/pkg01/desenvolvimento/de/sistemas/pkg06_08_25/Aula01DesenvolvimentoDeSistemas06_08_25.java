ca394e49e72fb494f433ffcc8883c15de8cb979d
Robert Germano <robertgermano77@gmail.com>
2025-08-06 20:38:32 -0300

    Add files via upload

---
+++ NetBeans - Aula 01 DS 06_08_25/Aula 01 - Desenvolvimento de Sistemas 06_08_25/src/aula/pkg01/desenvolvimento/de/sistemas/pkg06_08_25/Aula01DesenvolvimentoDeSistemas06_08_25.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula.pkg01.desenvolvimento.de.sistemas.pkg06_08_25;
+
+/**
+ *
+ * @author ROBERT_4924
+ */
+public class Aula01DesenvolvimentoDeSistemas06_08_25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Meu primeiro projeto NetBeans!");
+    }
+    
+}
